8a3cbfab43b12bc600722524bbe0a75db85143bb
Suyash gupta <89170907+Gsuyash86@users.noreply.github.com>
2023-10-08 07:31:27 +0000

    Completed

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,18 +1,24 @@
 import { useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { easing } from 'maath'
+import * as THREE from 'three'
 
 import {
   useGLTF,
   Environment,
   Center,
   AccumulativeShadows,
-  RandomizedLight
+  RandomizedLight,
+  useTexture,
+  Decal
 } from '@react-three/drei'
+import { useSnapshot } from 'valtio'
+import { state } from './store'
 
 export const App = ({ position = [0, 0, 2.5], fov = 25 }) => (
   <Canvas
     shadows
+    gl={{ preserveDrawingBuffer: true }}
     camera={{ position, fov }}
     eventSource={document.getElementById('root')}
     eventPrefix="client">
@@ -29,7 +35,15 @@ export const App = ({ position = [0, 0, 2.5], fov = 25 }) => (
 )
 
 function Shirt(props) {
+  const snap = useSnapshot(state)
+
+  const texture = useTexture(`/${snap.selectedDecal}.png`)
+
   const { nodes, materials } = useGLTF('/shirt_baked_collapsed.glb')
+
+  useFrame((state, delta) =>
+    easing.dampC(materials.lambert1.color, snap.selectedColor, 0.25, delta)
+  )
 
   return (
     <mesh
@@ -38,12 +52,30 @@ function Shirt(props) {
       material={materials.lambert1}
       material-roughness={1}
       {...props}
-      dispose={null}></mesh>
+      dispose={null}>
+      <Decal
+        position={[0, 0.04, 0.15]}
+        rotation={[0, 0, 0]}
+        scale={0.15}
+        opacity={0.7}
+        map={texture}
+        map-anisotropy={16}
+      />
+    </mesh>
   )
 }
 
 function Backdrop() {
   const shadows = useRef()
+
+  useFrame((state, delta) =>
+    easing.dampC(
+      shadows.current.getMesh().material.color,
+      state.selectedColor,
+      0.25,
+      delta
+    )
+  )
 
   return (
     <AccumulativeShadows
@@ -74,8 +106,10 @@ function Backdrop() {
 
 function CameraRig({ children }) {
   const group = useRef()
+  const snap = useSnapshot(state)
+
   useFrame((state, delta) => {
-    easing.damp3(state.camera.position, [0, 0, 2], 0.25, delta)
+    easing.damp3(state.camera.position, [snap.intro ? -state.viewport.width / 4 : 0, 0, 2], 0.25, delta)
     easing.dampE(
       group.current.rotation,
       [state.pointer.y / 10, -state.pointer.x / 5, 0],
